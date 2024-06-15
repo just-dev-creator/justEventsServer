d8c67b3cb20528badc4d1414c926cd04887a243f
Justus <justus@justcoding.tech>
2024-06-15 12:07:31 +0200

    Deactivated NPC Handler not needed anymore

--- justEvents-spigot/src/main/java/dev/just/justevents/utils/NPCHandler.java
+++ justEvents-spigot/src/main/java/dev/just/justevents/utils/NPCHandler.java
@@ -16,47 +16,47 @@ import java.util.Random;
 import java.util.UUID;
 
 public class NPCHandler implements Listener {
-    private static NPCPool pool;
-    private static final Random random = new Random();
-    public static void setup() {
-        pool = NPCPool.builder(Main.getPlugin(Main.class))
-                .spawnDistance(60)
-                .actionDistance(30)
-                .tabListRemoveTicks(20)
-                .build();
-        Bukkit.getPluginManager().registerEvents(new NPCHandler(), Main.getPlugin(Main.class));
-        registerAllNPCs();
-    }
-
-    public static void registerNPC(Location location, UUID skinUUID, String name) {
-        Profile profile = new Profile(skinUUID);
-        profile.complete();
-
-        profile.setName(name);
-        profile.setUniqueId(new UUID(random.nextLong(), 0));
-
-        NPC npc = NPC.builder()
-                .profile(profile)
-                .location(location)
-                .imitatePlayer(false)
-                .lookAtPlayer(true)
-                .build(pool);
-    }
-    private static void registerAllNPCs() {
-        registerEtbNCP();
-    }
-
-    private static void registerEtbNCP() {
-        NPCHandler.registerNPC(new Location(Bukkit.getWorld("world"), 0.5, 102, -34.5),
-                UUID.fromString("be4b5533-c56c-4b69-aa77-f80f75934b10"), ChatColor.BLUE + "EtB " +
-                        "Ep. 2");
-    }
-
-    @EventHandler
-    public void handleNPCInteract(PlayerNPCInteractEvent event) {
-        if (event.getNPC().getProfile().getName().equalsIgnoreCase(ChatColor.BLUE + "EtB " +
-                "Ep. 2")) {
-            ServerItems.connectToServer(event.getPlayer(), "ETB2");
-        }
-    }
+//    private static NPCPool pool;
+//    private static final Random random = new Random();
+//    public static void setup() {
+//        pool = NPCPool.builder(Main.getPlugin(Main.class))
+//                .spawnDistance(60)
+//                .actionDistance(30)
+//                .tabListRemoveTicks(20)
+//                .build();
+//        Bukkit.getPluginManager().registerEvents(new NPCHandler(), Main.getPlugin(Main.class));
+//        registerAllNPCs();
+//    }
+//
+//    public static void registerNPC(Location location, UUID skinUUID, String name) {
+//        Profile profile = new Profile(skinUUID);
+//        profile.complete();
+//
+//        profile.setName(name);
+//        profile.setUniqueId(new UUID(random.nextLong(), 0));
+//
+//        NPC npc = NPC.builder()
+//                .profile(profile)
+//                .location(location)
+//                .imitatePlayer(false)
+//                .lookAtPlayer(true)
+//                .build(pool);
+//    }
+//    private static void registerAllNPCs() {
+//        registerEtbNCP();
+//    }
+//
+//    private static void registerEtbNCP() {
+//        NPCHandler.registerNPC(new Location(Bukkit.getWorld("world"), 0.5, 102, -34.5),
+//                UUID.fromString("be4b5533-c56c-4b69-aa77-f80f75934b10"), ChatColor.BLUE + "EtB " +
+//                        "Ep. 2");
+//    }
+//
+//    @EventHandler
+//    public void handleNPCInteract(PlayerNPCInteractEvent event) {
+//        if (event.getNPC().getProfile().getName().equalsIgnoreCase(ChatColor.BLUE + "EtB " +
+//                "Ep. 2")) {
+//            ServerItems.connectToServer(event.getPlayer(), "ETB2");
+//        }
+//    }
 }
